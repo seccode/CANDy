b9dae0e2247b7ba3381d0dae4fb67c01156eaddc
seccode <42663376+seccode@users.noreply.github.com>
2024-11-27 21:28:02 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+from collections import Counter
 import zstandard as zstd
 
 def compress(s,comp):
+    mc=[m[0] for m in Counter(s).most_common()]
     c={
-        "A":chr(0),
-        "C":chr(1),
-        "G":chr(2),
-        "T":chr(3)
+        mc[0]:chr(0),
+        mc[1]:chr(1),
+        mc[2]:chr(2),
+        mc[3]:chr(3)
     }
     m=[c[_s] for _s in s]
     return comp.compress("".join(m).encode())
 
 if __name__=="__main__":
-    s=open("dna").read()
+    s=open("dna").read().replace("\n","")
 
     comp=zstd.ZstdCompressor(level=22)
     _b=comp.compress(s.encode())
